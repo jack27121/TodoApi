5d83cdc6cc1f12d11e8e06b9e69f6e17ce0956e3
jack <jackconradsen@gmail.com>
2022-08-26 00:16:08 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using ProductAPIVS.Handler;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using Microsoft.AspNetCore.Authentication;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
+builder.Services.AddAuthentication("BasicAuthentication").AddScheme<AuthenticationSchemeOptions,BasicAuthenticationHandler>("BasicAuthentication", null);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ handler/BasicAuthenticationHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using System.Text;
+using System.Net.Http.Headers;
+using TodoApi.Models;
+using System.Security.Claims;
+namespace ProductAPIVS.Handler
+{
+  public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>{
+    private readonly TodoContext _DBContext;
+    public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> option, ILoggerFactory logger,
+    UrlEncoder encoder, ISystemClock clock, TodoContext dbContext):base(option,logger,encoder,clock){
+      _DBContext = dbContext;
+    }
+    protected async override Task<AuthenticateResult> HandleAuthenticateAsync(){
+      if(!Request.Headers.ContainsKey("Authorization")){
+        return AuthenticateResult.Fail("No header found");
+      }
+
+      var _headervalue=AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+      var bytes=Convert.FromBase64String(_headervalue.Parameter);
+      string credentials=Encoding.UTF8.GetString(bytes);
+      if(!string.IsNullOrEmpty(credentials)){
+        string[] array = credentials.Split(":");
+        string username = array[0];
+        string password = array[1];
+
+        Console.WriteLine(username);
+        Console.WriteLine(password);
+
+        var user=this._DBContext.Users.FirstOrDefault(item=>item.Username==username && item.Password == password);
+        if(user==null){
+          return AuthenticateResult.Fail("UnAuthrorized");
+        }
+
+        var claim=new[]{new Claim(ClaimTypes.Name,username)};
+        var identity=new ClaimsIdentity(claim,Scheme.Name);
+        var principal=new ClaimsPrincipal(identity);
+        var ticket=new AuthenticationTicket(principal,Scheme.Name);
+        return AuthenticateResult.Success(ticket);
+      }else{
+        return AuthenticateResult.Fail("Header blank or null");
+      }
+    }
+
+  }
+}
